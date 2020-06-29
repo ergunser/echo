cf2d14208364fcb192cb240cb8d56d3e0b4bf850
erguns <serkan.ergun@commencis.com>
2020-06-29 21:31:34 +0300

    List: Fetch item list

--- app/src/main/java/com/betbull/echo/main/MainViewModel.kt
+++ app/src/main/java/com/betbull/echo/main/MainViewModel.kt
@@ -1,11 +1,44 @@
 package com.betbull.echo.main
 
 import com.betbull.echo.base.viewmodel.BaseViewModel
+import com.betbull.echo.main.model.Api
+import com.betbull.echo.main.model.ResponseObject
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class MainViewModel : BaseViewModel() {
 
+    @Inject
+    lateinit var api: Api
 
-    //FIXME add api request
+    init {
+        fetchItemList()
+    }
+
+    private fun fetchItemList() {
+
+        api.fetchItemsByName()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+
+                handleResponse(it)
+
+            }, {
+
+                handleError()
+
+            }).autoDispose()
+
+    }
+
+    private fun handleResponse(responseObject: ResponseObject?) {
+        //FIXME handle response
+    }
+
+    private fun handleError() {
+        //FIXME handle Error
+    }
 
 }
