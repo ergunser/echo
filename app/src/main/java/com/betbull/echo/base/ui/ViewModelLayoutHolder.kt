ergunser/browser

Add Generic recycler view adapter & viewholder

app/src/main/java/com/softtech/browser/base/ui/ViewModelLayoutHolder.kt
@@ -1,0 +1,11 @@
+package com.softtech.browser.base.ui
+
+import com.softtech.browser.base.viewmodel.RecyclerItemViewModel
+
+interface ViewModelLayoutHolder {
+
+    val viewModel: RecyclerItemViewModel
+
+    val layoutResId: Int
+}
+

app/src/main/java/com/softtech/browser/base/ui/ViewModelViewHolder.kt
@@ -1,0 +1,31 @@
+package com.softtech.browser.base.ui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.RecyclerView
+import com.softtech.browser.base.viewmodel.RecyclerItemViewModel
+
+open class ViewModelViewHolder(
+    parent: ViewGroup,
+    @LayoutRes layoutId: Int,
+    private val viewModelVariableBindingId: Int,
+    protected val binding: ViewDataBinding = DataBindingUtil.inflate(
+        LayoutInflater.from(parent.context),
+        layoutId,
+        parent,
+        false
+    )
+) : RecyclerView.ViewHolder(binding.root) {
+
+    protected lateinit var viewModel: RecyclerItemViewModel
+
+    fun bind(viewModel: RecyclerItemViewModel) {
+        this.viewModel = viewModel
+        this.binding.setVariable(viewModelVariableBindingId, viewModel)
+        this.binding.executePendingBindings()
+    }
+
+}

app/src/main/java/com/softtech/browser/base/viewmodel/RecyclerItemViewModel.kt
@@ -1,0 +1,3 @@
+package com.softtech.browser.base.viewmodel
+
+abstract class RecyclerItemViewModel

ergunser/echo

app/src/main/java/com/betbull/echo/base/ui/ViewModelLayoutHolder.kt
@@ -1,0 +1,11 @@
+package com.betbull.echo.base.ui
+
+import com.betbull.echo.base.viewmodel.RecyclerItemViewModel
+
+interface ViewModelLayoutHolder {
+
+    val viewModel: RecyclerItemViewModel
+
+    val layoutResId: Int
+}
+

app/src/main/java/com/betbull/echo/base/ui/ViewModelViewHolder.kt
@@ -1,0 +1,31 @@
+package com.betbull.echo.base.ui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.RecyclerView
+import com.betbull.echo.base.viewmodel.RecyclerItemViewModel
+
+open class ViewModelViewHolder(
+    parent: ViewGroup,
+    @LayoutRes layoutId: Int,
+    private val viewModelVariableBindingId: Int,
+    protected val binding: ViewDataBinding = DataBindingUtil.inflate(
+        LayoutInflater.from(parent.context),
+        layoutId,
+        parent,
+        false
+    )
+) : RecyclerView.ViewHolder(binding.root) {
+
+    protected lateinit var viewModel: RecyclerItemViewModel
+
+    fun bind(viewModel: RecyclerItemViewModel) {
+        this.viewModel = viewModel
+        this.binding.setVariable(viewModelVariableBindingId, viewModel)
+        this.binding.executePendingBindings()
+    }
+
+}

app/src/main/java/com/betbull/echo/base/viewmodel/RecyclerItemViewModel.kt
@@ -1,0 +1,3 @@
+package com.betbull.echo.base.viewmodel
+
+abstract class RecyclerItemViewModel