3da9b8f1f3856f9191d3c0525b73c8265a1d6dd8
erguns <serkan.ergun@commencis.com>
2020-06-29 21:37:51 +0300

    List: Add list screen layout & binding

--- app/src/main/java/com/betbull/echo/main/MainActivity.kt
+++ app/src/main/java/com/betbull/echo/main/MainActivity.kt
@@ -2,12 +2,26 @@ package com.betbull.echo.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
 import com.betbull.echo.R
+import com.betbull.echo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel as MainViewModel
     }
 }
