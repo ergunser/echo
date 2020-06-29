ergunser/browser

Add ResourceUtil

app/src/main/java/com/softtech/browser/base/util/ResourceUtil.kt
@@ -1,0 +1,17 @@
+package com.softtech.browser.base.util
+
+import android.content.Context
+import androidx.annotation.StringRes
+import com.softtech.browser.base.SofttechApp
+
+object ResourceUtil {
+
+    private val context: Context
+        get() = SofttechApp.getInstance()?.applicationContext!!
+
+    fun getString(@StringRes resId: Int) = context.getString(resId)
+
+    fun getString(@StringRes resId: Int, vararg formatArgs: Any) = context.getString(resId, *formatArgs)
+
+
+}

ergunser/echo

app/src/main/java/com/betbull/echo/base/util/ResourceUtil.kt
@@ -1,0 +1,17 @@
+package com.betbull.echo.base.util
+
+import android.content.Context
+import androidx.annotation.StringRes
+import com.betbull.echo.base.BetbullEchoApp
+
+object ResourceUtil {
+
+    private val context: Context
+        get() = BetbullEchoApp.getInstance()?.applicationContext!!
+
+    fun getString(@StringRes resId: Int) = context.getString(resId)
+
+    fun getString(@StringRes resId: Int, vararg formatArgs: Any) = context.getString(resId, *formatArgs)
+
+
+}