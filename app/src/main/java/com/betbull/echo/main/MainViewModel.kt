c790dc75e02619f5d6d9ee834cf8998223892c73
erguns <serkan.ergun@commencis.com>
2020-06-30 17:01:51 +0300

    List: Add progressbar visibility

--- app/src/main/java/com/betbull/echo/main/MainViewModel.kt
+++ app/src/main/java/com/betbull/echo/main/MainViewModel.kt
@@ -14,6 +14,7 @@ class MainViewModel : BaseViewModel() {
     lateinit var api: Api
 
     val list = MutableLiveData<MutableList<ListItemViewHolder>>()
+    val progressBarVisible = MutableLiveData<Boolean>().apply { value = true }
 
     init {
         fetchItemList()
@@ -45,9 +46,13 @@ class MainViewModel : BaseViewModel() {
         }?.mapTo(list) { ListItemViewHolder(it) }
 
         this.list.value = list
+
+        progressBarVisible.value = false
     }
 
     private fun handleError() {
+        progressBarVisible.value = false
+
         //FIXME handle Error
     }
 
